acc9fc5e62777f13c72ddacb96586d90df10023f
Svet-lik <svet-st@yandex.ru>
2021-08-20 18:02:31 +0300

    Day4: flats

--- js/main.js
+++ js/main.js
@@ -3,37 +3,53 @@ $(document).ready(function () {
   const counterUp = $('.counter-up');
   const counterDown = $('.counter-down');
   const floorPath = $('.home-image path');
-// функция подкрашивания этажа
+  const modal = $('.modal');
+  const modalCloseButton = $('.modal-close-button');
+const viewFlatsButton =$('.view-flats');
+
+  // функция подкрашивания этажа
   const checkFloor = (usFloor) => {
-    usCurrentFloor = usFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+    usCurrentFloor = usFloor.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    });
     $('.counter').text(usCurrentFloor);
     floorPath.removeClass('current-floor');
     $(`[data-floor=${usCurrentFloor}]`).addClass('current-floor');
   }
 
+
+  const toggleModal = () => {
+    modal.toggleClass('is-open');
+  }
+
+  floorPath.on('click', toggleModal);
+  modalCloseButton.on('click', toggleModal);
+
   // функция при наведении мышки на этаж
-  floorPath.on('mouseover', function () {    
+  floorPath.on('mouseover', () => {
     currentFloor = $(this).attr('data-floor');
     $('.counter').text(currentFloor);
-    checkFloor(currentFloor);
+    floorPath.removeClass('current-floor');
+    // checkFloor(currentFloor);
   });
 
   // срабатывание кнопки вверх
-  counterUp.on('click', function () {
+  counterUp.on('click', () => {
     if (currentFloor < 18) {
       currentFloor++;
       checkFloor(currentFloor);
     }
   });
-// срабатывание кнопки вниз
-  counterDown.on('click', function () {
+  // срабатывание кнопки вниз
+  counterDown.on('click', () => {
     if (currentFloor > 2) {
       currentFloor--;
       checkFloor(currentFloor);
-      }
+    }
   });
 
-
+viewFlatsButton.on('click', toggleModal);
 
 
 });
